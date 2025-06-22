15b307b8ac133134bbe6683b56423afa9dfa9692
Alex <yoyobq@hotmail.com>
2025-06-22 15:58:44 +0800

    refactor(CollapseTrigger): 使用 Antd Button 替换自定义样式方便主题切换

--- src/components/CollapseTrigger/CollapseTrigger.tsx
+++ src/components/CollapseTrigger/CollapseTrigger.tsx
@@ -1,27 +1,24 @@
 // components/CollapseTrigger.tsx
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 import React from 'react';
 import styles from './CollapseTrigger.module.css';
 import type { CollapseTriggerProps } from './types';
 
 const CollapseTrigger: React.FC<CollapseTriggerProps> = ({
   collapsed,
-  collapsedWidth = '3.75rem',
-  expandedWidth = '15rem',
   onToggle,
 }) => {
-  const left = typeof (collapsed ? collapsedWidth : expandedWidth) === 'number'
-    ? `${collapsed ? collapsedWidth : expandedWidth}px`
-    : (collapsed ? collapsedWidth : expandedWidth);
 
   return (
-    <div
+    <Button
       className={styles.collapseTrigger}
-      style={{ left }}
       onClick={onToggle}
-    >
-      {collapsed ? <RightOutlined /> : <LeftOutlined />}
-    </div>
+      type="default"
+      shape="circle"
+      size="small"
+      icon={collapsed ? <RightOutlined /> : <LeftOutlined />}
+    />
   );
 };
 
